fba1c925425b6f12c4aa94d549f81d77631f3a4f
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2025-11-11 19:03:08 +0500

    Fix Login filename case

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import CustomerProfile from "./pages/CustomerProfile";
 import ManageUsers from "./pages/ManageUsers";
 import StockReport from "./pages/StockReport";
 import BarcodePrint from "./pages/BarcodePrint";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import InvoiceEdit from "./pages/InvoiceEdit";
 
 // ✅ NEW IMPORTS
